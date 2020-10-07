09c969fe356e92632fb5f96816ba256bd353ca24
Uetal <uetal@me.com>
2020-10-07 13:41:04 +0300

    Initial commit

---
+++ src/main/java/crud/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package crud.repository;
+
+import crud.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
---
+++ src/main/java/crud/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package crud.repository;
+
+import crud.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    @Query("SELECT user from User user where user.username = :name")
+    User findByUsername(@Param("name") String username);
+}
